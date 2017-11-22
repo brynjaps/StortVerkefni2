a7e0355552fbd416e01f01abe0c8ee027ad2f293
= <=>
2017-11-22 00:43:29 +0000

    changed folder structure

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # StortVerkefni2
+
+## Folder Structure
+We split the scss and the javascript files into two separate folders, style and js. 
+
+## Git Ignore
+On top of the common ignores like node_modules and .DS_Store we ignore all build files and output from both babel and scss.
+
+## Build
+Clone the project from github and run the build command with npm.
+```sh
+npm run build
+```
+
+## Developement
+To run in development mode - live reload and automatic compilation of both scss and js, run the dev command.
+```sh
+npm run dev
+```
--- js/myndband.js
+++ js/myndband.js
@@ -1,21 +1,10 @@
 // constant url to vide.json
-const videoJsonUrl = "videos.json";
-
-document.addEventListener('DOMContentLoaded', () => {
-  /*
-  Þegar myndband er valið er farið á aðra síðu með auðkenni á myndbandi í querystring, t.d.
-  video.html?id=1 . Sá vefur grípur hvert það myndband er og birtir það með titil þess í
-  haus og stýringum:
-  */
-  const videoParam = new URLSearchParams(window.location.search);
-  const video = new Video();
-  video.init(videoParam.get('id'));
-});
+const videoJsonUrl = 'videos.json';
 
 class Video {
   constructor() {
     this.isPlaying = false;
-    // binda foll
+    // bind functions
     this.onPressPlay = this.onPressPlay.bind(this);
     this.onPressMute = this.onPressMute.bind(this);
     this.onPressNext = this.onPressNext.bind(this);
@@ -33,13 +22,14 @@ class Video {
       // parse the response
       const res = JSON.parse(request.response);
       // try to find the video by id
-      const infoResult = res.videos.find(video => video.id == this.id);
+      const intId = parseInt(this.id, 10);
+      const infoResult = res.videos.find(video => video.id === intId);
       // call the setInfo with the result
       this.setInfo(infoResult);
     };
     // register to the onerror event
     request.onerror = () => {
-    }
+    };
     // send the request
     request.send();
   }
@@ -58,14 +48,18 @@ class Video {
     Meðan vídeó er ekki að spila er sýnt overlay með play takka í miðju og gegnsæum
     bakgrunn ( rgba(0, 0, 0, 0.2) í fyrirmynd).
   */
-  onPressPlay(e) {
+  onPressPlay() {
+    // get the play button
     const btn = document.querySelector('.play-button > img');
+    // if the video is not paused then pause it
+    // and show overlay
+    // else play it
     if (!this.video.paused) {
-      btn.src = "./img/pause.svg";
+      btn.src = './img/pause.svg';
       this.video.pause();
       document.querySelector('.overlay').classList.remove('hidden');
     } else {
-      btn.src = "./img/play.svg";
+      btn.src = './img/play.svg';
       this.video.play();
       document.querySelector('.overlay').classList.add('hidden');
     }
@@ -75,7 +69,10 @@ class Video {
   sekúndur eða á enda
   */
   onPressNext() {
-    this.video.currentTime = (this.video.currentTime + 3) > this.video.duration ? this.video.duration : (this.video.currentTime + 3);
+    this.video.currentTime = (
+      (this.video.currentTime + 3) > this.video.duration ? 
+        this.video.duration : 
+        (this.video.currentTime + 3));
   }
 
   /*
@@ -83,14 +80,17 @@ class Video {
   */
   onPressMute(e) {
     if (this.video.volume !== 0) {
-      e.target.src = "./img/unmute.svg";
+      e.target.src = './img/unmute.svg';
       this.video.volume = 0;
     } else {
-      e.target.src = "./img/mute.svg";
+      e.target.src = './img/mute.svg';
       this.video.volume = 1;
     }
   }
-
+  /*
+    Fullscreen takki, setur vídeó í fullscreen (athuga þarf stuðning og gera ráðstafanir með
+    requestFullscreen API)
+  */
   onPressFullscreen() {
     if (this.video.requestFullscreen) {
       video.requestFullscreen
@@ -100,26 +100,29 @@ class Video {
   }
 
   setInfo(info) {
+    // Ef vídeó er ekki til ( id er ekki í videos.json ) er skilaboð um það birt.
+    if (!info) {
+      document.querySelector('.video-title').innerText = 'Video finnst ekki';
+      const infoEl = document.querySelector('.info');
+      infoEl.innerHTML = '<p>Þetta video er ekki til, farðu til baka.</p>';
+      infoEl.classList.remove('hidden');
+      document.querySelector('button').addEventListener('click', () => { window.location.href = "/" })
+      return;
+    }
+    this.bindControls();
     // set the title
     document.querySelector('.video-title').innerText = info.title;
-    
     this.video.poster = info.poster;
     // add the source
     this.video.src = info.video;
     // when we can start to play we do so
     this.video.oncanplay = () => {
       this.video.play();
-    }
+    };
   }
 
-  initVideo() {
+  bindControls() {
     this.video = document.querySelector('video');
-  }
-
-  init(id) {
-    this.id = id;
-    this.initVideo();
-    this.onLoadMetaData();
     const controls = document.querySelector('.controls');
     controls.querySelector('.play-button').addEventListener('click', this.onPressPlay);
     controls.querySelector('.mute-button').addEventListener('click', this.onPressMute);
@@ -128,7 +131,23 @@ class Video {
     controls.querySelector('.next-button').addEventListener('click', this.onPressNext);
     controls.querySelector('.fullscreen-button').addEventListener('click', this.onPressFullscreen);
     document.querySelector('video').addEventListener('click', this.onPressPlay);
-    document.querySelector('button').addEventListener('click', () => { window.location.href = "/" })
+    document.querySelector('button').addEventListener('click', () => { window.location.href = '/'; });
+  }
+
+  init(id) {
+    this.id = id;
+    this.onLoadMetaData();
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  /*
+  Þegar myndband er valið er farið á aðra síðu með auðkenni á myndbandi í querystring, t.d.
+  video.html?id=1 . Sá vefur grípur hvert það myndband er og birtir það með titil þess í
+  haus og stýringum:
+  */
+  const videoParam = new URLSearchParams(window.location.search);
+  const video = new Video();
+  video.init(videoParam.get('id'));
+});
 
